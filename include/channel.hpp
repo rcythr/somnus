dab255627cb0d5338ebefdbfa5df00254a8be935
Richard Laughlin <rwl3564@rit.edu>
2014-08-10 22:48:31 -0400

    Implemented channels for pointer types and non-pointer types. Implemented test3 for pointer type.

--- include/channel.hpp
+++ include/channel.hpp
@@ -9,8 +9,72 @@
 
 namespace somnus
 {
+    template<typename T, class Enable = void>
+    struct Channel;
+
+    // Channel for pointer types.
     template<typename T>
-    struct Channel
+    struct Channel<T, typename std::enable_if<std::is_pointer<T>::value>::type>
+    {
+        T pop()
+        {
+            _m.lock();
+            if(!_queue.empty())
+            {
+                void* data = _queue.front();
+                _queue.pop();
+
+                _m.unlock();
+
+                return static_cast<T>(data);
+            }
+            else
+            {
+                std::shared_ptr<Actor> actor = this_actor();
+                std::shared_ptr<Task> task = this_task();
+
+                _waiting_tasks.push(std::tie(actor, task));
+
+                _m.unlock();
+
+                return static_cast<T>(yield());
+            }
+        }
+
+        void push(T t)
+        {
+            _m.lock();
+
+            if(!_waiting_tasks.empty())
+            {
+                auto actor_task = _waiting_tasks.front();
+                _waiting_tasks.pop();
+
+                _m.unlock();
+                
+                auto actor = std::get<0>(actor_task);
+                auto task = std::get<1>(actor_task);
+
+                set_yield_data(task, t);
+                actor->run(task);
+                return;
+            }
+            else
+            {
+                _queue.push(t);
+                _m.unlock();
+                return;
+            }
+        }
+    private:
+        std::mutex _m;
+        std::queue<void*> _queue;
+        std::queue<std::tuple<std::shared_ptr<Actor>, std::shared_ptr<Task>>> _waiting_tasks;
+    };
+
+    // Channel for non-pointer types.
+    template<typename T>
+    struct Channel<T, typename std::enable_if<!std::is_pointer<T>::value>::type>
     {
 
         T pop()
@@ -74,9 +138,7 @@ namespace somnus
                 return;
             }
         }
-
     private:
-
         std::mutex _m;
         std::queue<void*> _queue;
         std::queue<std::tuple<std::shared_ptr<Actor>, std::shared_ptr<Task>>> _waiting_tasks;
